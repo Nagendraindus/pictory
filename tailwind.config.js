017d05d4eefae32d9faca4d214d090fbda9fb1b8
Nagendraindus <nknagendra2k19@gmail.com>
2023-08-20 16:11:04 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily:{
+        squarePeg:["'Tektur', cursive"]
       },
+      colors:{
+        primary: {
+          main:"#4477CE",
+          light:"#982176",
+          veryLight:"#9BABB8",
+          dark:"#3E001F",
+          contrastText:"#FFE5AD",
+        }
+      }
     },
   },
   plugins: [],
